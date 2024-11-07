89d99670b1416eee89f3c3d5dd244a2f8525b87b
sskeymaster <102697909+sskeymaster@users.noreply.github.com>
2024-11-07 22:02:20 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ let gameChar = {
 	gameChar_head: 40
 };
 
+let gameItem = {
+	item_x: 400,
+	item_y: 400
+};
 
 function setup()
 {
@@ -76,134 +80,181 @@ function draw()
 	fill(0, 0, 0, 100);
 	rect(110, 422, 130, 140);
 	
-		//5. a collectable token - eg. a jewel, fruit, coins
+		//5. a collectable item
 		//lantern
-	stroke(0, 0, 0);
-	strokeWeight(7);
-	fill(212, 166, 55);
-	rect(400, 400, 25, 22);
-	fill(0,0,0);
-	triangle(398, 400, 427, 400, 412, 380);
-	strokeWeight(10);
-	point(412, 375);
+	Item();
+	
 	console.log(key);
 	
-	if (key == 'A')
+	if (keyIsDown(65)) //character moves
 	{
-			//Character - LEFT
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x - 10, gameChar.gameChar_y); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
-		fill(169, 169, 169);
-		rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100, 5, 42); //right leg
-		fill(255, 250, 250);
-		rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100, 5, 42); //left leg;
-		
-		Previous_key = 'A';
+		left();
+		gameChar.gameChar_x -= 5;
+		if (gameChar.gameChar_x <= 0)
+			{
+				gameChar.gameChar_x = 5;
+			}
+
 	}
 	else if (Previous_key == 'A' && key == ' ')
 	{
-	   		//Character - LEFT JUMP
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x - 10, gameChar.gameChar_y - 50); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100  - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100  - 50); // dress
-		fill(169, 169, 169);
-		rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100  - 50, 5, 21); //right leg
-		rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100  - 50 + 21, 21, 5);
-		fill(255, 250, 250);
-		rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100  - 50, 5, 21); //left leg
-		rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100  - 50 + 21, 21, 5); //left leg
+	   	left_jump();
 	}
-	else if (key == 'D')
+	else if (keyIsDown(68))
 	{
-		   //Character - RIGHT
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x + 10, gameChar.gameChar_y); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
-		fill(169, 169, 169);
-		rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100, 5, 42); //right leg
-		fill(255, 250, 250);
-		rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100, 5, 42); //left leg
+		right();
+		gameChar.gameChar_x += 5;
+		if (gameChar.gameChar_x >= 1024)
+			{
+				gameChar.gameChar_x = 1019;
+			}
 		
-		Previous_key = 'D'; 
 	}
 	else if (Previous_key == 'D' && key == ' ')
 	{
-			//Character - RIGHT JUMP
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x + 10, gameChar.gameChar_y - 50); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100 - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100 - 50); // dress
-		fill(169, 169, 169);
-		rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100 - 50, 5, 21); //right leg
-		rect(gameChar.gameChar_x - 2 - 16, gameChar.gameChar_y + 100 - 50 + 21, 21, 5); //right leg
-		fill(255, 250, 250);
-		rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100 - 50, 5, 21); //left leg
-		rect(gameChar.gameChar_x + 2 - 16, gameChar.gameChar_y + 100 - 50 + 21, 21, 5); //left leg
+		right_jump();
 	}
 	else if (Previous_key == '' && key == ' ')
 	{
-			//Character - FRONT JUMP
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x + 10, gameChar.gameChar_y - 50); //right eye
-		point(gameChar.gameChar_x - 10, gameChar.gameChar_y - 50); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100  - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100  - 50); // dress
-		fill(169, 169, 169);
-		rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100 - 50 + 21, 5, 7); //shadow left leg
-		rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100 - 50 + 21, 5, 7); //shadow right leg
-		fill(255, 250, 250);
-		rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100 - 50, 5, 21); //left leg
-		rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100 - 50, 5, 21); //right leg
+		front_jump();
 	}
 	else
 	{
-		//Character - FRONT
-		stroke(255, 250, 250);
-		strokeWeight(gameChar.gameChar_head);
-		point(gameChar.gameChar_x, gameChar.gameChar_y); //head
-		stroke(0, 0, 0);
-		strokeWeight(10);
-		point(gameChar.gameChar_x + 10, gameChar.gameChar_y); //right eye
-		point(gameChar.gameChar_x - 10, gameChar.gameChar_y); //left eye
-		noStroke();
-		fill(255, 250, 250);
-		triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
-		rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100, 5, 42); //left leg
-		rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100, 5, 42); //right leg
-		
+		front();
 		Previous_key = '';
-	
 	}
-	
 
 }
 
+
+function Item() //lantern, collectable
+{
+	stroke(0, 0, 0);
+	strokeWeight(7);
+	fill(212, 166, 55);
+	rect(gameItem.item_x, gameItem.item_y, 25, 22);
+	fill(0,0,0);
+	triangle(gameItem.item_x - 2, gameItem.item_y, gameItem.item_x + 27, gameItem.item_y, gameItem.item_x + 12, gameItem.item_y - 20);
+	strokeWeight(10);
+	point(gameItem.item_x + 12, gameItem.item_y - 25);
+}
+
+
+function left()
+{
+		//Character - LEFT
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x - 10, gameChar.gameChar_y); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
+	fill(169, 169, 169);
+	rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100, 5, 42); //right leg
+	fill(255, 250, 250);
+	rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100, 5, 42); //left leg;
+}
+
+
+function left_jump()
+{
+		//Character - LEFT JUMP
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x - 10, gameChar.gameChar_y - 50); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100  - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100  - 50); // dress
+	fill(169, 169, 169);
+	rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100  - 50, 5, 21); //right leg
+	rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100  - 50 + 21, 21, 5);
+	fill(255, 250, 250);
+	rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100  - 50, 5, 21); //left leg
+	rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100  - 50 + 21, 21, 5); //left leg
+}
+
+
+function right()
+{
+		//Character - RIGHT
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x + 10, gameChar.gameChar_y); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
+	fill(169, 169, 169);
+	rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100, 5, 42); //right leg
+	fill(255, 250, 250);
+	rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100, 5, 42); //left leg
+}
+
+
+function right_jump()
+{
+		//Character - RIGHT JUMP
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x + 10, gameChar.gameChar_y - 50); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100 - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100 - 50); // dress
+	fill(169, 169, 169);
+	rect(gameChar.gameChar_x - 2, gameChar.gameChar_y + 100 - 50, 5, 21); //right leg
+	rect(gameChar.gameChar_x - 2 - 16, gameChar.gameChar_y + 100 - 50 + 21, 21, 5); //right leg
+	fill(255, 250, 250);
+	rect(gameChar.gameChar_x + 2, gameChar.gameChar_y + 100 - 50, 5, 21); //left leg
+	rect(gameChar.gameChar_x + 2 - 16, gameChar.gameChar_y + 100 - 50 + 21, 21, 5); //left leg
+}
+
+
+function front_jump()
+{
+		//Character - FRONT JUMP
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y - 50); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x + 10, gameChar.gameChar_y - 50); //right eye
+	point(gameChar.gameChar_x - 10, gameChar.gameChar_y - 50); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15 - 50, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100  - 50, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100  - 50); // dress
+	fill(169, 169, 169);
+	rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100 - 50 + 21, 5, 7); //shadow left leg
+	rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100 - 50 + 21, 5, 7); //shadow right leg
+	fill(255, 250, 250);
+	rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100 - 50, 5, 21); //left leg
+	rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100 - 50, 5, 21); //right leg
+}
+
+
+function front()
+{
+		//Character - FRONT
+	stroke(255, 250, 250);
+	strokeWeight(gameChar.gameChar_head);
+	point(gameChar.gameChar_x, gameChar.gameChar_y); //head
+	stroke(0, 0, 0);
+	strokeWeight(10);
+	point(gameChar.gameChar_x + 10, gameChar.gameChar_y); //right eye
+	point(gameChar.gameChar_x - 10, gameChar.gameChar_y); //left eye
+	noStroke();
+	fill(255, 250, 250);
+	triangle(gameChar.gameChar_x, gameChar.gameChar_y + 15, gameChar.gameChar_x - 30, gameChar.gameChar_y + 100, gameChar.gameChar_x + 30, gameChar.gameChar_y + 100); // dress
+	rect(gameChar.gameChar_x - 10, gameChar.gameChar_y + 100, 5, 42); //left leg
+	rect(gameChar.gameChar_x + 5, gameChar.gameChar_y + 100, 5, 42); //right leg
+}
